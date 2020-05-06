b612029681d5dfb71657fbf24352dd28bc9cdc94
jhilton11 <scolaguys@yahoo.co.uk>
2020-05-06 21:29:40 +0100

    made some updates

--- index.js
+++ index.js
@@ -47,3 +47,13 @@ function loadData() {
         }
     });
 }
+
+function scroll() {
+    $(".navbar a").click(function(){
+        $("body,html").animate({
+         scrollTop:$("#" + $(this).data('value')).offset().top
+        },1000)
+        
+       })
+      
+}
